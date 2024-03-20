f7e565402ea12546b1eb0cc0e3a48522c2d2b787
ZoeAcero <zar.amazona@gmail.com>
2024-03-20 11:42:35 +0100

    a

---
+++ src/main/java/Editor_Texto_Interactivo/Creacion_Almacenamiento.java
@@ -0,0 +1,9 @@
+package Editor_Texto_Interactivo;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Creacion_Almacenamiento {
+}
