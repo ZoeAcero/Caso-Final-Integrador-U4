c12a3679652c5236dd34345aa4a33d563c5f6f62
ZoeAcero <zar.amazona@gmail.com>
2024-04-03 11:29:29 +0200

    a

--- src/main/java/Interfaz_Grafica_Avanzada/VentanaMultiple.java
+++ src/main/java/Interfaz_Grafica_Avanzada/VentanaMultiple.java
@@ -1,14 +1,19 @@
 package Interfaz_Grafica_Avanzada;
 
-import jdk.jfr.Event;
-
 import javax.swing.*;
 import java.awt.*;
 
 public class VentanaMultiple {
     public static void main(String[] args) {
         EventQueve.invokeLater(() -> {
-            
+            try {
+                JFrame frame1 = crearVentana("Ventana 1", 100, 100);
+                JFrame frame2 = crearVentana("Ventana 2", 500, 100);
+                JFrame frame3 = crearVentana("Ventana 3", 700, 100);
+
+                
+            }
+
         });
     }
 }
