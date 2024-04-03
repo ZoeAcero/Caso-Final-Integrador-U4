ca52ac26d126ae49ce0822b51b5a6ab9cd199b00
ZoeAcero <zar.amazona@gmail.com>
2024-04-03 12:09:31 +0200

    a

--- src/main/java/Interfaz_Grafica_Avanzada/BarraDesplazamiento.java
+++ src/main/java/Interfaz_Grafica_Avanzada/BarraDesplazamiento.java
@@ -2,5 +2,14 @@ package Interfaz_Grafica_Avanzada;
 
 import javax.swing.*;
 
-public class BarraDesplazamiento {
+public class BarraDesplazamiento extends JFrame{
+    public BarraDesplazamiento() {
+        JTextArea areaDeTexto = new JTextArea(20, 20);
+        JScrollPane scrollPane = new JScrollPane(areaDeTexto);
+
+        add(scrollPane);
+
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
