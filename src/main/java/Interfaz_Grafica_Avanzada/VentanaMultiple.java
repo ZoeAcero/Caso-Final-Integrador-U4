c4ab808682c99cca0e7ade3093b22b2609ba5c01
ZoeAcero <zar.amazona@gmail.com>
2024-04-03 11:26:03 +0200

    a

---
+++ src/main/java/Interfaz_Grafica_Avanzada/VentanaMultiple.java
@@ -0,0 +1,14 @@
+package Interfaz_Grafica_Avanzada;
+
+import jdk.jfr.Event;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class VentanaMultiple {
+    public static void main(String[] args) {
+        EventQueve.invokeLater(() -> {
+            
+        });
+    }
+}
