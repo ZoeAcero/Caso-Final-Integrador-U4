ed5a86b468d8afc040d439905b06b21222cf9470
ZoeAcero <zar.amazona@gmail.com>
2024-04-03 11:59:56 +0200

    a

--- src/main/java/Interfaz_Grafica_Avanzada/SeguimientoRaton.java
+++ src/main/java/Interfaz_Grafica_Avanzada/SeguimientoRaton.java
@@ -5,5 +5,10 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class SeguimientoRaton extends JFrame {
-    
+    private JLabel etiquetaPosicionRaton;
+
+    public SeguimientoRaton() {
+        etiquetaPosicionRaton = new JLabel();
+        add(etiquetaPosicionRaton, BorderLayout.SOUTH);
+    }
 }
