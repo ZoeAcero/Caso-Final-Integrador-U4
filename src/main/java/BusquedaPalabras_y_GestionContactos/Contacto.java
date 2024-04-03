1db88cc6bfed4a62afd155d42b0f4fcca3aefa39
ZoeAcero <zar.amazona@gmail.com>
2024-04-03 10:55:30 +0200

    a

--- src/main/java/BusquedaPalabras_y_GestionContactos/Contacto.java
+++ src/main/java/BusquedaPalabras_y_GestionContactos/Contacto.java
@@ -4,4 +4,10 @@ public class Contacto {
     private String nombre;
     private String telefono;
     private String email;
+
+    public Contacto(String nombre, String telefono, String email){
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
 }
