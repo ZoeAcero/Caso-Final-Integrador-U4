bc83684a8435c4e470b3c426a2cf4bf8bd959433
ZoeAcero <zar.amazona@gmail.com>
2024-04-09 20:18:14 +0200

    a

---
+++ src/main/java/Editor_Texto_Interactivo/EditorTextoInteractivo.java
@@ -0,0 +1,15 @@
+package Editor_Texto_Interactivo;
+import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.io.*;
+
+public class EditorTextoInteractivo extends JFrame{
+    private JTextArea areaTexto;
+
+
+    public
+
+
+}
