3b34bf4a9d8d70174e4ba450f64b06764efae0a0
ZoeAcero <zar.amazona@gmail.com>
2024-04-09 20:20:08 +0200

    a

--- src/main/java/Editor_Texto_Interactivo/EditorTextoInteractivo.java
+++ src/main/java/Editor_Texto_Interactivo/EditorTextoInteractivo.java
@@ -2,14 +2,67 @@ package Editor_Texto_Interactivo;
 import javax.swing.JFrame;
 import java.awt.*;
 import javax.swing.*;
-import java.awt.event.*;
+import java.util.Scanner;
 import java.io.*;
 
 public class EditorTextoInteractivo extends JFrame{
     private JTextArea areaTexto;
 
 
-    public
+    public EditorTextoInteractivo() {
+        areaTexto = new JTextArea();
+        JScrollPane scrollPane = new JScrollPane(areaTexto);
+        add(scrollPane, BorderLayout.CENTER);
 
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menuArchivo = new JMenu("Archivo");
+        JMenuItem guardarItem = new JMenuItem("Guardar");
+        JMenuItem abrirItem = new JMenuItem("Abrir");
 
+        guardarItem.addActionListener(e -> guardarArchivo());
+        abrirItem.addActionListener(e -> abrirArchivo());
+
+        menuArchivo.add(guardarItem);
+        menuArchivo.add(abrirItem);
+        menuBar.add(menuArchivo);
+        setJMenuBar(menuBar);
+
+        setSize(500, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void guardarArchivo() {
+        JFileChooser fileChooser = new JFileChooser();
+        if (fileChooser.showSaveDialog(this) == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            try (PrintWriter out = new PrintWriter(file)) {
+                out.print(areaTexto.getText());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void abrirArchivo() {
+        JFileChooser fileChooser = new JFileChooser();
+        if (fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            try (Scanner scanner = new Scanner(file)) {
+                while (scanner.hasNextLine()) {
+                    areaTexto.append(scanner.nextLine() + "\n");
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            EditorTextoInteractivo frame = new EditorTextoInteractivo();
+            frame.setVisible(true);
+        });
+    }
 }
+
+
