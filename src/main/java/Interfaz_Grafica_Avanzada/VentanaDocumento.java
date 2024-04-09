f1197108dfaad21fa390854d056c9a7b7677a34a
ZoeAcero <zar.amazona@gmail.com>
2024-04-09 20:51:21 +0200

    a

--- src/main/java/Interfaz_Grafica_Avanzada/VentanaDocumento.java
+++ src/main/java/Interfaz_Grafica_Avanzada/VentanaDocumento.java
@@ -1,4 +1,30 @@
 package Interfaz_Grafica_Avanzada;
 
-public class VentanaDocumento {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+public class VentanaDocumento extends JFrame {
+    private JTextArea areaTexto;
+    private JLabel etiquetaPosicion;
+
+    public VentanaDocumento() {
+        areaTexto = new JTextArea();
+        JScrollPane scrollPane = new JScrollPane(areaTexto);
+        add(scrollPane, BorderLayout.CENTER);
+
+        etiquetaPosicion = new JLabel();
+        add(etiquetaPosicion, BorderLayout.SOUTH);
+
+        areaTexto.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                etiquetaPosicion.setText("Posición del ratón: " + e.getX() + ", " + e.getY());
+            }
+        });
+
+        setSize(500, 500);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
 }
