45ad202284266e3a6360501890728594a2f08102
ZoeAcero <zar.amazona@gmail.com>
2024-04-09 17:51:38 +0200

    a

--- src/main/java/Interfaz_Grafica_Avanzada/HerramientaDibujo.java
+++ src/main/java/Interfaz_Grafica_Avanzada/HerramientaDibujo.java
@@ -8,5 +8,17 @@ public class HerramientaDibujo extends JFrame{
     private Point puntoInicio = null;
     private Point puntoFin = null;
 
+    public HerramientaDibujo() {
+        JPanel panelDibujo = new JPanel();
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            if (puntoInicio != null && puntoFinal != null) {
+                g.drawLine(puntoInicio.x, puntoInicio.y, puntoFinal.x, puntoFinal.y);
+            }
+        }
+    };
+
     
+        }
+
 }
