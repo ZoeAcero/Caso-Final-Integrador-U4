b18eb008b0db0d8c481dab884c5461aea4b79e7f
ZoeAcero <zar.amazona@gmail.com>
2024-04-09 18:07:16 +0200

    a

--- src/main/java/Interfaz_Grafica_Avanzada/HerramientaDibujo.java
+++ src/main/java/Interfaz_Grafica_Avanzada/HerramientaDibujo.java
@@ -10,40 +10,56 @@ public class HerramientaDibujo extends JFrame{
 
     public HerramientaDibujo() {
         JPanel panelDibujo = new JPanel();
-        public void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            if (puntoInicio != null && puntoFinal != null) {
-                g.drawLine(puntoInicio.x, puntoInicio.y, puntoFinal.x, puntoFinal.y);
+            @Override
+            public void paintComponent(Graphics DebugGraphics g) {
+                super.paintComponents(g);
+                if (puntoInicio != null && puntoFinal != null) {
+                    g.drawLine(puntoInicio.x, puntoInicio.y, puntoFinal.x, puntoFinal.y);
+                }
             }
-        }
-    };
+        };
 
-panelDibujo.addMouseMotionListener(new MouseAdapter() {
-    public void mousePressed(MouseEvent e) {
-        puntoInicio = new Point(e.getX() , e.getY());
-            puntoFinal = puntoInicio;
-            repaint();
-        }
+        panelDibujo.addMouseMotionListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                puntoInicio = new Point(e.getX() , e.getY());
+                puntoFinal = puntoInicio;
+                repaint();
+            }
 
-        public void mouseReleased(MouseEvent e) {
-            puntoFinal = null;
-            puntoInicio = null;
-        }
-    });
+            public void mouseReleased(MouseEvent e) {
+                puntoFinal = null;
+                puntoInicio = null;
+            }
+        });
 
-    panelDibujo.addMouseMotionListener(new MouseAdapter() {
-        public void mouseDragged(MouseEvent e) {
-            puntoFinal = new Point(e.getX(), e.getY());
-            repaint();
-        }
-    });
 
-    
+        panelDibujo.addMouseMotionListener(new MouseAdapter() {
+            public void mouseDragged(MouseEvent e) {
+                puntoFinal = new Point(e.getX(), e.getY());
+                repaint();
+            }
+        });
+
+        add(panelDibujo);
+
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            try {
+                HerramientaDibujo frame = new HerramientaDibujo();
+                frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 
 
 
 
-        }
+
 
 
