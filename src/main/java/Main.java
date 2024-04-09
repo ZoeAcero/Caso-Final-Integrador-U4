33f1eda7b986baf9669b1bbd94aa9012e9d21276
ZoeAcero <zar.amazona@gmail.com>
2024-04-09 18:46:03 +0200

    a

---
+++ src/main/java/Main.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import BusquedaPalabras_y_GestionContactos.*;
+import Interfaz_Grafica_Avanzada.*;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import Editor_Texto_Interactivo.*;
+import Comparador_y_Contador_de_Contenido.*;
+
+
+public class Main () {
+
+    JMenuBar menuBar = new JMenuBar();
+
+
+    //menus para cada una de las opciones
+
+    JMenu menuEditorTexto = new JMenu("Editor de Texto");
+    JMenu menuBusquedaGestion = new JMenu("Busqueda de Palabras y Gestion de Contactos");
+    JMenu menuInterfazGrafica = new JMenu("Interfaz Grafica Avanzada");
+    JMenu menuComparadorContenido = new JMenu("Comparador y Contador de Contenido");
+
+
+    //agregar menus a barra de menu
+
+    menuBar.add(menuEditorTexto);
+    menuBar.add(menuBusquedaGestion);
+    menuBar.add(menuInterfazGrafica);
+    menuBar.add(menuComparadorContenido);
+
+
+    //agregar barra menu a marco principal
+
+    setJMenuBar(menuBar);
+
+
+    //editor texto interactivo
+
+    JMenuItem itemEditorTexto = new JMenuItem("Editor de Texto");
+
+    itemEditorTexto.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            EditorTextoGUI editorTextoGUI = new EditorTextoGUI();
+            editorTextoGUI.setVisible(true);
+        }
+    });
+    menuEditorTexto.add(itemEditorTexto);
+}
