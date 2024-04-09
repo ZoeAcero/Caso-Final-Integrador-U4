d53be25b18dfe1b1f99ffaa8df0f671cd898c3b1
ZoeAcero <zar.amazona@gmail.com>
2024-04-09 17:47:47 +0200

    a

---
+++ src/main/java/Interfaz_Grafica_Avanzada/HerramientaDibujo.java
@@ -0,0 +1,12 @@
+package Interfaz_Grafica_Avanzada;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class HerramientaDibujo extends JFrame{
+    private Point puntoInicio = null;
+    private Point puntoFin = null;
+
+    
+}
