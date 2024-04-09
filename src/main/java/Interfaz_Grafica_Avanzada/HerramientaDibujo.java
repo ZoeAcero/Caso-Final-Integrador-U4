124b583102c8c234c4dda6b54e264fcd132499fa
ZoeAcero <zar.amazona@gmail.com>
2024-04-09 18:12:57 +0200

    a

--- src/main/java/Interfaz_Grafica_Avanzada/HerramientaDibujo.java
+++ src/main/java/Interfaz_Grafica_Avanzada/HerramientaDibujo.java
@@ -11,7 +11,7 @@ public class HerramientaDibujo extends JFrame{
     public HerramientaDibujo() {
         JPanel panelDibujo = new JPanel();
             @Override
-            public void paintComponent(Graphics DebugGraphics g) {
+            protected void paintComponent(Graphics DebugGraphics g) {
                 super.paintComponents(g);
                 if (puntoInicio != null && puntoFinal != null) {
                     g.drawLine(puntoInicio.x, puntoInicio.y, puntoFinal.x, puntoFinal.y);
@@ -19,47 +19,12 @@ public class HerramientaDibujo extends JFrame{
             }
         };
 
-        panelDibujo.addMouseMotionListener(new MouseAdapter() {
-            public void mousePressed(MouseEvent e) {
-                puntoInicio = new Point(e.getX() , e.getY());
-                puntoFinal = puntoInicio;
-                repaint();
-            }
+         panelDibujo.addMouseMotionListener(new MouseAdapter() {
+        @Override
+        public void mousePressed(MouseEvent e) {
+            puntoInicio = new Point(e.getX(), e.getY());
+            puntoFinal = puntoInicio;
+            panelDibujo.repaint();
+        }
 
-            public void mouseReleased(MouseEvent e) {
-                puntoFinal = null;
-                puntoInicio = null;
-            }
-        });
-
-
-        panelDibujo.addMouseMotionListener(new MouseAdapter() {
-            public void mouseDragged(MouseEvent e) {
-                puntoFinal = new Point(e.getX(), e.getY());
-                repaint();
-            }
-        });
-
-        add(panelDibujo);
-
-        setSize(300, 200);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            try {
-                HerramientaDibujo frame = new HerramientaDibujo();
-                frame.setVisible(true);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-    }
-
-
-
-
-
-
-
+        
